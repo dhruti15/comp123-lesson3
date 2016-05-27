4794aaedeaf22e2fadc0cbd90002d39b462ae859
Dhrutiben Patel <dpate483@my.centennialcollege.ca>
2016-05-27 15:55:54 -0400

    added person class

---
+++ comp123-lesson3/person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace comp123_lesson3
+{
+    class person
+    {
+    }
+}
