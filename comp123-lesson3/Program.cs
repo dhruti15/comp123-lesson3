8227cea7bc5175947af691472a4daf58b1631e1b
Dhrutiben Patel <dpate483@my.centennialcollege.ca>
2016-05-27 16:58:51 -0400

    update person class to include name property and constructor

--- comp123-lesson3/Program.cs
+++ comp123-lesson3/Program.cs
@@ -8,8 +8,26 @@ namespace comp123_lesson3
 {
   public class Program
     {
+        /**
+        *the main method for our driver class program
+        *
+        *@method Main
+        *@param {string[]} args 
+        */
        public static void Main(string[] args)
         {
+            // create a new instance of the person class
+            person person = new person("DHRUTI");
+            Console.WriteLine(person.Name);
+
+            //set a value in the name property of the person class
+            person.Name = "Dhruti";
+
+            //get the name value and print the result to console
+            Console.WriteLine(person.Name);
+            
+
         }
+        
     }
 }
--- comp123-lesson3/person.cs
+++ comp123-lesson3/person.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace comp123_lesson3
 {
-    class person
+    /*this class defines a generic person
+    *
+    *@class person
+    *@field _name {string}
+    *
+    */
+    public class person
     {
+        // private instance variables ++++++++++++++++++++++++++++++++++++++
+       private string _name;
+
+        //public properties ++++++++++++++++++++++++++++++++++++++++
+
+            /*
+            *this is a property for our _name field
+            *@property {string} Name
+            */
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+        }
+        /**
+        *this is the default empty constructer for the person class
+        *
+        *@constructor person
+        */
+
+        public person()
+        {
+            this._name ="unknown name!";
+        }
+        /**
+        *this is the  constructer that takes name asc a parameter and passes it to the _name private instace variable
+        *
+        *@constructor person
+        *@param {string} name
+        */
+
+        public person(string name)
+        {
+            this._name = name;
+        }
     }
 }
